34bcea9afefaa2fadd43df7e446610c001280a8a
Đorđe Cvijić <djordjec@nordeus.eu>
2015-05-03 13:30:08 +0200

    [sphinxgame1] 22-51 8.12.2012 cvile - sredio pozicije frejmova tako da sprajtovi ne budu onako iseceni - Player klasi stavio leftStandingSprite i rightStandingSprite, kao i promenljivu oldState koja pamti smer poslednjeg kretanja, i na osnovu toga se koristi neka od dva "Standing" sprajta

---
+++ sphinxgame1/sphinxgame1/sphinxgame1/Player.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace sphinxgame1
+{
+    class Player
+    {
+        private Sprite leftRunningSprite;
+        private Sprite rightRunningSprite;
+        private Sprite leftStandingSprite;
+        private Sprite rightStandingSprite;
+        private enum SpriteStates { LEFT, RIGHT, STANDING };
+        SpriteStates spriteState = SpriteStates.STANDING;
+        SpriteStates oldState = SpriteStates.RIGHT;
+        private Rectangle areaLimit;
+        private Vector2 velocity;
+        private Vector2 location;
+        
+
+        public Player(Texture2D texture,
+            Rectangle leftFrame,
+            Rectangle rightFrame,
+            Rectangle leftStandingFrame,
+            Rectangle rightStandingFrame,
+            int frameNumber,
+            Vector2 initialLocation,
+            int screenWidth,
+            int screenHeight)
+        {
+            rightRunningSprite = new Sprite(texture,
+                initialLocation,
+                Vector2.Zero,
+                rightFrame);
+            leftRunningSprite = new Sprite(texture,
+                initialLocation,
+                Vector2.Zero,
+                leftFrame);
+            leftStandingSprite = new Sprite(texture,
+                 initialLocation,
+                 Vector2.Zero,
+                 leftStandingFrame);
+            rightStandingSprite = new Sprite(texture,
+                initialLocation,
+                Vector2.Zero,
+                rightStandingFrame);
+
+            leftRunningSprite.Scale = rightRunningSprite.Scale = leftStandingSprite.Scale = rightStandingSprite.Scale = 2.0f;
+            leftRunningSprite.FrameTime = rightRunningSprite.FrameTime = leftStandingSprite.FrameTime = rightStandingSprite.FrameTime = 0.1f;
+
+            areaLimit = new Rectangle(0, 0, screenWidth, 390);
+
+            for (int i = 1; i < frameNumber; i++)
+            {
+                leftRunningSprite.addFrame(new Rectangle(leftFrame.X + i * 24,
+                     leftFrame.Y,
+                     leftFrame.Width,
+                     leftFrame.Height));
+                rightRunningSprite.addFrame(new Rectangle(rightFrame.X - i * 24,
+                     rightFrame.Y,
+                     rightFrame.Width,
+                     rightFrame.Height));
+            }
+
+            location = initialLocation;
+            velocity = Vector2.Zero;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (spriteState == SpriteStates.LEFT) leftRunningSprite.Draw(spriteBatch);
+            else if (spriteState == SpriteStates.RIGHT) rightRunningSprite.Draw(spriteBatch);
+            else if (oldState == SpriteStates.LEFT) leftStandingSprite.Draw(spriteBatch);
+            else rightStandingSprite.Draw(spriteBatch);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            bool leftPressed = Keyboard.GetState().IsKeyDown(Keys.Left);
+            bool rightPressed = Keyboard.GetState().IsKeyDown(Keys.Right);
+
+            if (leftPressed && !rightPressed)
+            {
+                oldState = spriteState = SpriteStates.LEFT;
+                velocity = new Vector2(-200, 0);
+            }
+
+            else if (rightPressed && !leftPressed)
+            {
+                oldState = spriteState = SpriteStates.RIGHT;
+                velocity = new Vector2(200, 0);
+            }
+            else
+            {
+                spriteState = SpriteStates.STANDING;
+                velocity = Vector2.Zero;
+            }
+
+            location += velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+
+            if (spriteState == SpriteStates.LEFT)
+            {
+                leftRunningSprite.Location = location;
+                leftRunningSprite.Update(gameTime);
+            }
+            else if (spriteState == SpriteStates.RIGHT)
+            {
+                rightRunningSprite.Location = location;
+                rightRunningSprite.Update(gameTime);
+            }
+            else
+            {
+                rightStandingSprite.Location = leftStandingSprite.Location = location;
+            }
+
+        }
+    }
+}
---
+++ sphinxgame1/sphinxgame1/sphinxgame1/Sprite.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace sphinxgame1
+{
+    class Sprite
+    {
+        private Texture2D texture;
+        private Vector2 location;
+        private Vector2 velocity;
+        private int currentFrame = 0;
+        private List<Rectangle> frames = new List<Rectangle>();
+        private float frameTime = 0.1f;
+        private float timeForCurrentFrame = 0.0f;
+        private int frameWidth;
+        private int frameHeight;
+        private float scale = 1.0f;
+
+        public Sprite(Texture2D texture,
+            Vector2 location,
+            Vector2 velocity,
+            Rectangle initialFrame)
+        {
+            this.texture = texture;
+            this.location = location;
+            this.velocity = velocity;
+            frames.Add(initialFrame);
+            frameWidth = initialFrame.Width;
+            frameHeight = initialFrame.Height;
+        }
+
+        public void addFrame(Rectangle newFrame)
+        {
+            frames.Add(newFrame);
+        }
+
+        public float Scale
+        {
+            get { return scale; }
+            set { scale = value; }
+        }
+
+        public Texture2D Texture
+        {
+            get { return texture; }
+            set { texture = value; }
+        }
+
+        public Vector2 Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+
+        public Vector2 Velocity
+        {
+            get { return velocity; }
+            set { velocity = value; }
+        }
+
+        public int CurrentFrame
+        {
+            get { return currentFrame; }
+            set { currentFrame = value; }
+        }
+
+        public float FrameTime
+        {
+            get { return frameTime; }
+            set { frameTime = value; }
+        }
+
+        public int FrameWidth
+        {
+            get { return frameWidth; }
+            set { frameWidth = value; }
+        }
+
+        public int FrameHeight
+        {
+            get { return frameHeight; }
+            set { frameHeight = value; }
+        }
+
+        public Vector2 Center
+        {
+            get { return location + new Vector2(frameWidth / 2, frameHeight / 2); }
+        }
+
+        public Rectangle Source
+        {
+            get { return frames[currentFrame]; }
+        }
+
+        public Rectangle Destination
+        {
+            get
+            {
+                return new Rectangle((int)location.X,
+                    (int)location.Y,
+                    frameWidth,
+                    frameHeight);
+            }
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            timeForCurrentFrame += elapsed;
+            if (timeForCurrentFrame >= frameTime)
+            {
+                currentFrame = (currentFrame + 1) % frames.Count;
+                timeForCurrentFrame = 0.0f;
+            }
+            location += (velocity * elapsed);
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture,
+                Center,
+                Source,
+                Color.White,
+                0.0f,
+                new Vector2(frameWidth / 2, frameHeight / 2),
+                scale,
+                SpriteEffects.None,
+                0.0f);
+        }
+    }
+}
